8e8af4284fe1e463c69a20a55fac5a1b73c1f0f7
Ahmed Araby <ahmedaraby605@gmail.com>
2021-08-12 17:09:49 +0200

    generate some executable files and add some comments:

--- intro/io.c
+++ intro/io.c
@@ -6,15 +6,20 @@
 #include <sys/types.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
+    // representation of the file from the disc in the memory
+    // this do not mean that all the file content is loaded in memory.
+    // this function can not go into folders
     int fd = open("/tmp/file", O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
     assert(fd >= 0);
+    // printf("file descriptor is : %d ", fd);
     char buffer[20];
     sprintf(buffer, "hello world\n");
     int rc = write(fd, buffer, strlen(buffer));
     assert(rc == (strlen(buffer)));
     fsync(fd);
     close(fd);
+    printf("done");
     return 0;
 }
-
